1df6db9148e5569d72fb03e6343ac6e029331234
Saransh Saini <saranshsss99@gmail.com>
2022-04-02 17:20:28 +0530

    updated 02/04

--- todo-react-app/src/App.jsx
+++ todo-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import AddTodo from "./components/AddTodo";
 import Header from "./components/Header";
+import Todos from "./components/Todos";
 function App() {
   return (
     <>
       <Header />
       <AddTodo />
+      <Todos />
     </>
   );
 }
---
+++ todo-react-app/src/components/Todos.jsx
@@ -0,0 +1,10 @@
+import Todo from "./Todo";
+const Todos = () => {
+  return (
+    <section className="todos">
+      <Todo />
+    </section>
+  );
+};
+
+export default Todos;
